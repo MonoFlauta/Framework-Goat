779772f7d3da3e34bf9db77fd152bde06407bc83
Facundo Balboa <monoflauta@gmail.com>
2018-05-29 13:19:30 -0300

    Added a finite state machine

---
+++ Framework Goat/Assets/Framework Goat/FiniteStateMachine/FiniteStateMachine.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace FrameworkGoat.FiniteStateMachine
+{
+    public class FiniteStateMachine<T>
+    {
+        private T _owner;
+        private Dictionary<System.Type, State<T>> _states;
+        private State<T> _currentState;
+
+        /// <summary>
+        /// Creates a Finite State Machine
+        /// </summary>
+        /// <param name="owner">The owner of the state machine</param>
+        public FiniteStateMachine(T owner)
+        {
+            _owner = owner;
+        }
+        
+        /// <summary>
+        /// Adds a state
+        /// </summary>
+        /// <param name="state">State to add</param>
+        public void AddState(State<T> state)
+        {
+            state.SetStateMachine(this);
+            _states.Add(state.GetType(), state);
+        }
+
+        /// <summary>
+        /// Exits the current state and, if it exists, sets the new state and calls its enter
+        /// </summary>
+        /// <typeparam name="TS">Type of state to set</typeparam>
+        public void SetState<TS>() where TS : State<T>
+        {
+            if (_currentState != null)
+                _currentState.Exit();
+            if (_states.ContainsKey(typeof(TS)))
+            {
+                _currentState = _states[typeof(TS)];
+                _currentState.Enter();
+            }
+        }
+
+        /// <summary>
+        /// Updates the current state
+        /// </summary>
+        public void Update()
+        {
+            if(_currentState!=null)
+                _currentState.Update();
+        }
+
+        /// <summary>
+        /// Late updates the current state
+        /// </summary>
+        public void LateUpdate()
+        {
+            if (_currentState != null)
+                _currentState.LateUpdate();
+        }
+    }
+}
---
+++ Framework Goat/Assets/Framework Goat/FiniteStateMachine/State.cs
@@ -0,0 +1,46 @@
+﻿namespace FrameworkGoat.FiniteStateMachine
+{
+    public abstract class State<T>
+    {
+        protected T _owner;
+        protected FiniteStateMachine<T> _stateMachine;
+
+        /// <summary>
+        /// Constructor of the State
+        /// </summary>
+        /// <param name="owner">Owner of the state</param>
+        public State(T owner)
+        {
+            _owner = owner;
+        }
+
+        /// <summary>
+        /// Sets the Finite State Machine that owns this state. Automatically called from Finite State Machine when added the state.
+        /// </summary>
+        /// <param name="sm">The finite state machine</param>
+        public void SetStateMachine(FiniteStateMachine<T> sm)
+        {
+            _stateMachine = sm;
+        }
+
+        /// <summary>
+        /// Called when entering the state
+        /// </summary>
+        public abstract void Enter();
+
+        /// <summary>
+        /// Called to update the state
+        /// </summary>
+        public abstract void Update();
+
+        /// <summary>
+        /// Called to late update the state
+        /// </summary>
+        public abstract void LateUpdate();
+
+        /// <summary>
+        /// Called to exit the state
+        /// </summary>
+        public abstract void Exit();
+    }
+}
