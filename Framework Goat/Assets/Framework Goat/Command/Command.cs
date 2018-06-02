e74c386799e9a001638c01c3056201bdb2b95261
Facundo Balboa <monoflauta@gmail.com>
2018-06-02 18:35:57 -0300

    Added Command

---
+++ Framework Goat/Assets/Framework Goat/Command/Command.cs
@@ -0,0 +1,14 @@
+﻿namespace FrameworkGoat.Command
+{
+    public abstract class Command
+    {
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        public abstract void Execute();
+        /// <summary>
+        /// UnExecutes the command
+        /// </summary>
+        public abstract void UnExecute();
+    }
+}
