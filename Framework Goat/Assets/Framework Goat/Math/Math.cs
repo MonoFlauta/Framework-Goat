92688efc1e7943c2996723b4628bb7ff171add91
Facundo Balboa <monoflauta@gmail.com>
2018-06-03 18:52:48 -0300

    Added a Math utility class

---
+++ Framework Goat/Assets/Framework Goat/Math/Math.cs
@@ -0,0 +1,72 @@
+﻿namespace FrameworkGoat.Math
+{
+
+    public class Math
+    {
+        /// <summary>
+        /// Gets the factorial of a number
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <returns>The factorial</returns>
+        public static int GetFactorial(int value)
+        {
+            if (value <= 1) return 1;
+            return value * GetFactorial(value - 1);
+        }
+
+        /// <summary>
+        /// Gets the factorial of a number
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <returns>The factorial</returns>
+        public static int GetFactorial(float value)
+        {
+            return GetFactorial((int)value);
+        }
+
+        /// <summary>
+        /// Gets the Fibonacci number of a number
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <returns>The Fibonacci number</returns>
+        public static int GetFibonacci(int value)
+        {
+            if (value == 0) return 0;
+            if (value <= 2) return 1;
+            return GetFibonacci(value - 1) + GetFibonacci(value - 2);
+        }
+
+        /// <summary>
+        /// Gets the Fibonacci number of a number
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <returns>The Fibonacci number</returns>
+        public static int GetFibonacci(float value)
+        {
+            return GetFactorial((int)value);
+        }
+
+        /// <summary>
+        /// Gets the binomial coefficient from two numbers
+        /// </summary>
+        /// <param name="firstValue">First number</param>
+        /// <param name="secondValue">Second number</param>
+        /// <returns>The binomial coefficient of two numbers</returns>
+        public static int GetBinomialCoefficient(int firstValue, int secondValue)
+        {
+            if (firstValue == secondValue || secondValue == 0) return 1;
+            return GetFactorial(firstValue) / (GetFactorial(secondValue) * GetFactorial(firstValue - secondValue));
+        }
+
+        /// <summary>
+        /// Gets the binomial coefficient from two numbers
+        /// </summary>
+        /// <param name="firstValue">First number</param>
+        /// <param name="secondValue">Second number</param>
+        /// <returns>The binomial coefficient of two numbers</returns>
+        public static int GetBinomialCoefficient(float firstValue, float secondValue)
+        {
+            return GetBinomialCoefficient((int)firstValue, (int)secondValue);
+        }
+    }
+}
