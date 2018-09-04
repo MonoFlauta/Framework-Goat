dacd18d74b663df5bdc2c8cfd09218943d826661
Facundo Balboa <monoflauta@gmail.com>
2018-09-04 20:48:35 -0300

    Added a Layer Controller

---
+++ Framework Goat/Assets/Framework Goat/UnityUtilities/2D/LayerController/LayerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+namespace FrameworkGoat
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class LayerController : MonoBehaviour
+    {
+        public SortingOrder initialSortingOrder = SortingOrder.Y;
+
+        private Func<int> _updateCallback;
+        private SpriteRenderer _spriteRenderer;
+
+        void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            SetSortingOrder(initialSortingOrder);
+        }
+
+        void Update()
+        {
+            _spriteRenderer.sortingOrder = _updateCallback();
+        }
+
+        /// <summary>
+        /// Sets the sorting order by using the enum options
+        /// </summary>
+        /// <param name="sortingOrder">Sorting order</param>
+        public void SetSortingOrder(SortingOrder sortingOrder)
+        {
+            switch(sortingOrder)
+            {
+                case SortingOrder.X:
+                    _updateCallback = () => { return (int)transform.position.x; };
+                    break;
+                case SortingOrder.Y:
+                    _updateCallback = () => { return (int)transform.position.y; };
+                    break;
+                case SortingOrder.Z:
+                    _updateCallback = () => { return (int)transform.position.z; };
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Sets the sorting order by using a Func
+        /// </summary>
+        /// <param name="sortingOrder">Sorting Order</param>
+        public void SetSortingOrder(Func<int> sortingOrder)
+        {
+            _updateCallback = sortingOrder;
+        }
+
+        public enum SortingOrder
+        {
+            X,
+            Y,
+            Z
+        }
+    }
+}
