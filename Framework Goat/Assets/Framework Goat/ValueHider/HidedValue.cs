3e34eac9fdebcbf05ffa98e7f61bfdeec985fc2d
Facundo Balboa <monoflauta@gmail.com>
2019-04-17 12:05:48 -0300

    Added Value Hider

---
+++ Framework Goat/Assets/Framework Goat/ValueHider/HidedValue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FrameworkGoat
+{
+    public class HidedValue<T>
+    {
+        private Func<T, T> _hideMethod;
+        private Func<T, T> _showMethod;
+        private T _value;
+
+        /// <summary>
+        /// Creates a hided value.
+        /// </summary>
+        /// <param name="hideMethod">Method used to hide</param>
+        /// <param name="showMethod">Method used to show</param>
+        public HidedValue(Func<T, T> hideMethod, Func<T, T> showMethod)
+        {
+            _hideMethod = hideMethod;
+            _showMethod = showMethod;
+            Value = default(T);
+        }
+
+        /// <summary>
+        /// Creates a hided value with an initial value
+        /// </summary>
+        /// <param name="hideMethod">Method used to hide</param>
+        /// <param name="showMethod">Method used to show</param>
+        /// <param name="initialValue">Initial value</param>
+        public HidedValue(Func<T, T> hideMethod, Func<T, T> showMethod, T initialValue)
+        {
+            _hideMethod = hideMethod;
+            _showMethod = showMethod;
+            Value = initialValue;
+        }
+
+        /// <summary>
+        /// Value
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                return _showMethod(_value);
+            }
+            set
+            {
+                _value = _hideMethod(value);
+            }
+        }
+    }
+}
