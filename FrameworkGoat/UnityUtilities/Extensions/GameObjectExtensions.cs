39ccaa06cd30e5c2a611efb6ff48d2d484b227fa
Facundo Balboa <monoflauta@gmail.com>
2021-07-06 20:06:09 -0300

    Added Destroy Children extensions for GameObject and Transform

---
+++ FrameworkGoat/UnityUtilities/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace FrameworkGoat
+{
+    public static class GameObjectExtensions
+    {
+        /// <summary>
+        /// Destroys all children of the game object
+        /// </summary>
+        /// <param name="gameObject">GameObject to use</param>
+        public static void DestroyChildren(this GameObject gameObject)
+        {
+            for (var i = gameObject.transform.childCount - 1; i >= 0; i--)
+            {
+                Object.Destroy(gameObject.transform.GetChild(i).gameObject);
+            }
+        }
+    }
+}
---
+++ FrameworkGoat/UnityUtilities/Extensions/TransformExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace FrameworkGoat
+{
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Destroys all children of the transform
+        /// </summary>
+        /// <param name="transform">Transform to use</param>
+        public static void DestroyChildren(this Transform transform)
+        {
+            for (var i = transform.childCount - 1; i >= 0; i--)
+            {
+                Object.Destroy(transform.GetChild(i).gameObject);
+            }
+        }
+    }
+}
