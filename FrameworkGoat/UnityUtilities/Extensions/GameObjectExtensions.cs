a6291a165fc54cb66873556ed5ba5ce450772b90
Facundo Balboa <monoflauta@gmail.com>
2021-07-08 17:38:55 -0300

    Get or add component

--- FrameworkGoat/UnityUtilities/Extensions/GameObjectExtensions.cs
+++ FrameworkGoat/UnityUtilities/Extensions/GameObjectExtensions.cs
@@ -21,5 +21,18 @@ namespace FrameworkGoat
         {
             gameObject.transform.ResetTransformation();
         }
+
+        /// <summary>
+        /// Gets, or adds if doesn't contain yet, a component
+        /// </summary>
+        /// <param name="gameObject">GameObject to use</param>
+        /// <typeparam name="T">The component type</typeparam>
+        /// <returns>Returns the component</returns>
+        public static T GetOrAddComponent<T>(this GameObject gameObject) where T : MonoBehaviour
+        {
+            var component = gameObject.GetComponent<T>();
+            if (component == null) gameObject.AddComponent<T>();
+            return component;
+        }
     }
 }
