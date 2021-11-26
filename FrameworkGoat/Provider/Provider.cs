b09f6b195b3a78b598493f337dc99c1de1fa8e1b
Facundo Balboa <monoflauta@gmail.com>
2021-11-26 18:09:55 -0300

    Added provider

---
+++ FrameworkGoat/Provider/Provider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Framework_Goat.FrameworkGoat.Provider
+{
+    public static class Provider
+    {
+        private static readonly Dictionary<Type, object> Objects = new Dictionary<Type, object>();
+
+        public static T GetOrInstantiate<T>(Func<T> factoryMethod)
+        {
+            if(!Objects.ContainsKey(typeof(T)))
+                Objects.Add(typeof(T), factoryMethod());
+            return (T)Objects[typeof(T)];
+        }
+
+        public static T Get<T>()
+        {
+            return (T)Objects[typeof(T)];
+        }
+
+        public static void Set<T>(T value)
+        {
+            Objects[typeof(T)] = value;
+        }
+
+        public static void ClearAll()
+        {
+            Objects.Clear();
+        }
+
+        public static void Clear<T>()
+        {
+            Objects.Remove(typeof(T));
+        }
+    }
+}
