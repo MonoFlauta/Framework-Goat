f11cd7f5f00d2d20f90f710febb039a09cd60673
Facundo Balboa <monoflauta@gmail.com>
2024-03-30 12:11:42 -0300

    Add Display Version

---
+++ FrameworkGoat/UnityUtilities/2D/DisplayVersion/TMPDisplayVersion.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Framework_Goat.FrameworkGoat.UnityUtilities._2D.DisplayVersion
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TMPDisplayVersion : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<TextMeshProUGUI>().text = Application.version;
+        }
+    }
+}
---
+++ FrameworkGoat/UnityUtilities/2D/DisplayVersion/TextDisplayVersion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Framework_Goat.FrameworkGoat.UnityUtilities._2D.DisplayVersion
+{
+    [RequireComponent(typeof(Text))]
+    public class TextDisplayVersion : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<Text>().text = Application.version;
+        }
+    }
+}
